da9448221e52c1d4bf8dc630f11f27079bc1b25e
Kuponiyi Oluwaponire <kponire@gmail.com>
2026-05-27 12:03:02 +0100

    fix: error page ui

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,16 +1,31 @@
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-  const error: any = useRouteError();
+  const error = useRouteError() as any;
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "50px",
+      }}
+    >
+      <h1>Oops! Something went wrong.</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error?.statusText || error?.message}</i>
+        <i>{error.statusText || error.message}</i>
       </p>
+      <a
+        href="/dashboard"
+        style={{ color: "#0070f3", textDecoration: "underline" }}
+      >
+        Back to Dashboard
+      </a>
     </div>
   );
 }
